3a9d254bf63c75c6422cea6a54574ad8cabca78d
Ajay Kumar <72861880+Ajay-Kumar-13@users.noreply.github.com>
2023-04-27 09:08:19 +0530

    Update route.js v10

--- backend/Routes/route.js
+++ backend/Routes/route.js
@@ -45,7 +45,7 @@ router.get("/checkSession", (req, res) => {
 router.get("/logout", (req, res) => {
     
     if (req.session.userName && req.cookies.user_sid) {
-//         res.clearCookie("user_sid", {domain: 'https://event-organization.onrender.com/',secure: true, sameSite: "none"});
+        res.clearCookie("user_sid");
         
         res.send({ logout: true });
     }
